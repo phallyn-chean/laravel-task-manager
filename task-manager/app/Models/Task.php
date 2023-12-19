5b56a107c62e263d9540f150bd47e494f681e619
dev8 <dev8@poscardigital.com>
2023-12-19 15:01:10 +0700

    add models

---
+++ task-manager/app/Models/Task.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'is_done',
+        'project_id'
+    ] ;
+
+    protected $casts = [
+        'is_done'=>'boolean'
+    ] ;
+
+    protected $hidden = [
+        'updated_at',
+    ];
+
+    public function creator():BelongsTo{
+        return $this->belongsTo(User::class,'creator_id');
+    }
+
+    public function project(): BelongsTo{
+        return $this->belongsTo(Project::class);
+    }
+
+    public static function booted(): void{
+        static::addGlobalScope('creator', function (Builder $builder) {
+            $builder->where('creator_id',Auth::id())
+                ->orWhereIn('project_id', Auth::user()->memberships->pluck('id'));
+        });
+    }
+}
--- task-manager/app/Models/User.php
+++ task-manager/app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +44,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function tasks(): HasMany{
+        return $this->hasMany(Task::class, 'creator_id');
+    }
+
+    public function projects(): HasMany{
+        return $this->hasMany(Project::class,'creator_id');
+    }
+
+    public function memberships(): BelongsToMany{
+        return $this->belongsToMany(Project::class, Member::class);
+    }
 }
